17e6928ce9a086cd677c54ba92bb5a73dbe058ef
maeve <maeve.revels@getg5.com>
2014-01-02 20:02:25 +0000

    Disable webmock after the test suite to support Code Climate

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -15,6 +15,10 @@ Dir[File.join(File.dirname(__FILE__), 'support', '*.rb')].each { |f| require f }
 
 RSpec.configure do |config|
   config.include FakeFS::SpecHelpers
+
+  # We need to explicitly disable webmock to report
+  # coverage data to Code Climate
+  config.after(:suite) { WebMock.disable! }
 end
 
 require 'g5_authentication_client'
