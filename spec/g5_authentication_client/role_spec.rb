783aa99bed01fa29bc441a5cc03d39d8a0e03561
Jonathan Samples <jonathan.samples@g5searchmarketing.com>
2015-05-20 14:44:24 -0700

    Added type and urn to role

--- spec/g5_authentication_client/role_spec.rb
+++ spec/g5_authentication_client/role_spec.rb
@@ -3,8 +3,16 @@ require 'spec_helper'
 describe G5AuthenticationClient::Role do
   subject(:role) { G5AuthenticationClient::Role.new(attributes) }
 
-  let(:attributes) { {name: name} }
+  let(:attributes) do {
+    name: name,
+    type: type,
+    urn: urn,
+  }
+  end
+
   let(:name) { 'awesome_role' }
+  let(:type) { 'SomeClass' }
+  let(:urn)  { 'some_urn' }
 
   context 'with default initialization' do
     let(:attributes) {}
@@ -12,11 +20,23 @@ describe G5AuthenticationClient::Role do
     it 'should have a nil name' do
       expect(role.name).to be_nil
     end
+    it 'has a nil type' do
+      expect(role.type).to be_nil
+    end
+    it 'has a nil urn' do
+      expect(role.urn).to be_nil
+    end
   end
 
   context 'with full initialization' do
     it 'should have the correct name' do
       expect(role.name).to eq(name)
+    end
+    it 'should have the correct name' do
+      expect(role.type).to eq(type)
+    end
+    it 'should have the correct name' do
+      expect(role.urn).to eq(urn)
     end
   end
 
@@ -52,6 +72,41 @@ describe G5AuthenticationClient::Role do
         expect { validate! }.to raise_error
       end
     end
+  end
+
+  describe '#validate_for_create!' do
+    subject(:validate_for_create) {role.validate_for_create!}
+
+    context 'when type not GLOBAL and no urn' do
+
+      let(:type) { 'not_global' }
+      let(:urn) {  }
+
+      it 'will raise an error' do
+        expect { validate_for_create }.to raise_error
+      end
+    end
+
+    context 'when type GLOBAL and no urn' do
+
+      let(:type) { 'GLOBAL' }
+      let(:urn) {  }
+
+      it 'will raise an error' do
+        expect { validate_for_create }.to_not raise_error
+      end
+    end
+
+    context 'when type not GLOBAL and existing urn' do
+
+      let(:type) { 'NOT_GLOBAL' }
+      let(:urn) { 'some_urn' }
+
+      it 'will raise an error' do
+        expect { validate_for_create }.to_not raise_error
+      end
+    end
+
   end
 
   describe '#to_hash' do
