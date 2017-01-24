1aed432c47be8747502fcbf6276028a3f3f532a1
bbauer <brian.bauer@g5platform.com>
2017-01-24 10:20:29 -0800

    Handle nil response

--- lib/g5_authentication_client/auth_token_helper.rb
+++ lib/g5_authentication_client/auth_token_helper.rb
@@ -3,7 +3,7 @@ module G5AuthenticationClient::AuthTokenHelper
   # Yield response should have a 'code' method for the http status code
   def do_with_username_pw_access_token
     response = yield cached_username_pw_access_token
-    if 401 == response.code.to_i
+    if response.nil? || 401 == response.code.to_i
       @cached_username_pw_access_token = nil
       response                         = yield cached_username_pw_access_token
     end
