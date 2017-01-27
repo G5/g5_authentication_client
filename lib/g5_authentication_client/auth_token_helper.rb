e9fff78767d65651ff15bc705bd93970eb56d0a8
David N. Welton <davidw@dedasys.com>
2017-01-27 15:58:34 -0800

    RestClient throws an exception on 401
    https://github.com/rest-client/rest-client#exceptions-see-httpwwww3orgprotocolsrfc2616rfc2616-sec10html

--- lib/g5_authentication_client/auth_token_helper.rb
+++ lib/g5_authentication_client/auth_token_helper.rb
@@ -2,12 +2,15 @@ module G5AuthenticationClient::AuthTokenHelper
   # Return response to 'yield'
   # Yield response should have a 'code' method for the http status code
   def do_with_username_pw_access_token
-    response = yield cached_username_pw_access_token
-    if 401 == response.code.to_i
-      @cached_username_pw_access_token = nil
-      response                         = yield cached_username_pw_access_token
+    begin
+      response = yield cached_username_pw_access_token
+    rescue RestClient::ExceptionWithResponse => e
+      if e.response.code.to_i == 401
+        @cached_username_pw_access_token = nil
+        response                         = yield cached_username_pw_access_token
+      end
+      response
     end
-    response
   end
 
   def cached_username_pw_access_token
